36081bf3919293796e50ae60462ebde51254ddb6
asyylz <49663592+asyylz@users.noreply.github.com>
2024-05-25 01:39:23 +0100

    update

--- Foodies/components/meals/image-picker.js
+++ Foodies/components/meals/image-picker.js
@@ -1,44 +1,58 @@
 'use client';
+
 import { useRef, useState } from 'react';
+import Image from 'next/image';
+
 import classes from './image-picker.module.css';
+
 export default function ImagePicker({ label, name }) {
-  const imageInput = useRef();
   const [pickedImage, setPickedImage] = useState();
+  const imageInput = useRef();
 
   function handlePickClick() {
     imageInput.current.click();
   }
+
   function handleImageChange(event) {
     const file = event.target.files[0];
+
     if (!file) {
       setPickedImage(null);
       return;
     }
+
     const fileReader = new FileReader();
-    fileReader.onLoad = () => {
+
+    fileReader.onload = () => {
       setPickedImage(fileReader.result);
     };
+
     fileReader.readAsDataURL(file);
   }
 
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
-      <div className={classes.cotrols}>
+      <div className={classes.controls}>
         <div className={classes.preview}>
           {!pickedImage && <p>No image picked yet.</p>}
           {pickedImage && (
-            <Image src={pickedImage} alt="The image selected by user." fill />
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user."
+              fill
+            />
           )}
         </div>
         <input
           className={classes.input}
           type="file"
           id={name}
-          accept="image/png,image/jpeg"
+          accept="image/png, image/jpeg"
           name={name}
           ref={imageInput}
           onChange={handleImageChange}
+          required
         />
         <button
           className={classes.button}
