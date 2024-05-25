76bd4fedc924b45f20cd3cc1854d29adbc07010d
asyylz <49663592+asyylz@users.noreply.github.com>
2024-05-25 11:32:55 +0100

    Update ShareMealPage to use useFormState and validate form input in shareMeal action

--- Foodies/app/meals/share/page.js
+++ Foodies/app/meals/share/page.js
@@ -1,8 +1,11 @@
+'use client';
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { useFormState } from 'react-dom';
 import { shareMeal } from '@/lib/actions';
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -12,7 +15,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -42,6 +45,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
+            {state.message && <p>{state.message}</p>}
             <MealsFormSubmit />
           </p>
         </form>
