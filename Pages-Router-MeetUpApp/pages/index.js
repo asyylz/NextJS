e9efb20581eb83616a4fba56534a1bc5c7977e19
asyylz <49663592+asyylz@users.noreply.github.com>
2024-05-27 18:30:28 +0100

    components

---
+++ Pages-Router-MeetUpApp/pages/index.js
@@ -0,0 +1,21 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image:
+      'https://cdn.mos.cms.futurecdn.net/5WUroWJ3ECE9pk9vBhXiqP-1200-80.png',
+    adress: 'Some address 1',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image:
+      'https://cdn.mos.cms.futurecdn.net/5WUroWJ3ECE9pk9vBhXiqP-1200-80.png',
+    adress: 'Some address 2',
+  },
+];
+export default function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
