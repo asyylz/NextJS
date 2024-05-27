ad8259eb24a9a77963d28361d5b69bb47527ab94
asyylz <49663592+asyylz@users.noreply.github.com>
2024-05-27 19:12:01 +0100

    Updated MainNavigation.js, MeetupItem.js, added [meetupId]/index.js, updated _app.js and index.js, and modified globals.css

---
+++ Pages-Router-MeetUpApp/pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail
+    image='https://cdn.mos.cms.futurecdn.net/5WUroWJ3ECE9pk9vBhXiqP-1200-80.png'
+    title='First Meetup'
+    address='Some Street 5, Some City'
+    description='This is a first meetup'
+  />
+  );
+}
