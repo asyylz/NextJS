cce4d02b31c80b8705a8009f3144afa86c25e588
asyylz <49663592+asyylz@users.noreply.github.com>
2024-05-28 05:36:01 +0100

    Added getStaticPaths and getStaticProps functions to MeetupDetails page, and updated MeetupDetail component props to use meetupData from getStaticProps

--- Pages-Router-MeetUpApp/pages/[meetupId]/index.js
+++ Pages-Router-MeetUpApp/pages/[meetupId]/index.js
@@ -3,10 +3,33 @@ import MeetupDetail from '../../components/meetups/MeetupDetail';
 export default function MeetupDetails() {
   return (
     <MeetupDetail
-    image='https://cdn.mos.cms.futurecdn.net/5WUroWJ3ECE9pk9vBhXiqP-1200-80.png'
-    title='First Meetup'
-    address='Some Street 5, Some City'
-    description='This is a first meetup'
-  />
+      image="https://cdn.mos.cms.futurecdn.net/5WUroWJ3ECE9pk9vBhXiqP-1200-80.png"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a first meetup"
+    />
   );
+}
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { meetupId: 'm1' } }, { params: { meetupId: 'm2' } }],
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          'https://cdn.mos.cms.futurecdn.net/5WUroWJ3ECE9pk9vBhXiqP-1200-80.png',
+        title: 'First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup',
+      },
+    },
+  };
 }
