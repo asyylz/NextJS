d3c4e2ba8ad0cbe4b2914727362718904cc151cd
asyylz <49663592+asyylz@users.noreply.github.com>
2024-05-28 06:09:17 +0100

    Added MongoDB dependency and implemented MongoDB connection in new-meetup API route

--- Pages-Router-MeetUpApp/pages/api/new-meetup.js
+++ Pages-Router-MeetUpApp/pages/api/new-meetup.js
@@ -1,6 +1,19 @@
-export default function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
-    const { image, title, address, description } = data;
+    const client = await MongoClient.connect(
+
+      'mongodb+srv://username:password@cluster0.diaashw.mongodb.net/meetups'
+    );
+    const db = client.db();
+    const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
+
+export default handler;
