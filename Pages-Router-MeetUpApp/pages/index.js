a2e1f4ecc9f699b4dccb9e9eae11f11737bee4c7
asyylz <49663592+asyylz@users.noreply.github.com>
2024-05-28 06:30:20 +0100

    Removed dummy meetups data, connected to MongoDB, and implemented API route for creating new meetups

--- Pages-Router-MeetUpApp/pages/index.js
+++ Pages-Router-MeetUpApp/pages/index.js
@@ -1,22 +1,6 @@
 import { useEffect, useState } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
-
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image:
-      'https://cdn.mos.cms.futurecdn.net/5WUroWJ3ECE9pk9vBhXiqP-1200-80.png',
-    adress: 'Some address 1',
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image:
-      'https://cdn.mos.cms.futurecdn.net/5WUroWJ3ECE9pk9vBhXiqP-1200-80.png',
-    adress: 'Some address 2',
-  },
-];
+import { MongoClient } from 'mongodb';
 export default function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
@@ -35,10 +19,25 @@ export default function HomePage(props) {
 // SSG
 
 export async function getStaticProps() {
+  const client = await MongoClient.connect(
+  
+     'mongodb+srv://username:password@cluster0.diaashw.mongodb.net/meetups'
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
-    revalidate: 10,
+    revalidate: 1,
   };
 }
